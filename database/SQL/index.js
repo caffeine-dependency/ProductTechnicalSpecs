d45629a3a6dc634172811354f213ea9caf9e3523
Sam Joseph <sjoseph92@gmail.com>
2019-07-27 14:56:17 -0700

    [feat] added db helper for inserting new techspec

--- database/SQL/index.js
+++ database/SQL/index.js
@@ -2,17 +2,17 @@ var pg = require('pg');
 var conString = "postgres://samjoseph@localhost:5432/pumpsquad2";
 
 var client = new pg.Client(conString);
-client.connect().then(() => console.log('connected')).catch((err) => console.error('err'));
+client.connect()
 
 const getByIdSQL = (id) => {
     return client.query(` explain analyze select * from techspecs where id=${id}`)
 }
 
-
-
-module.exports = {
-    getByIdSQL
+const addTechSpecSQL = (id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care) => {
+    return client.query(`INSERT INTO techspecs (id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care) VALUES (${id},${technicalFeatures},${designAndFit},${zippersAndFly},${pocketConfig},${construction},${collarConfig},${hemConfig},${fabricTreatment},${materials},${care})`)
 }
 
-    
-
+module.exports = {
+    getByIdSQL,
+    addTechSpecSQL
+}
