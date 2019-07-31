b97defa7c4ba90cf3419f68ed3a1a53e4f965439
Sam Joseph <sjoseph92@gmail.com>
2019-07-31 16:54:24 -0700

    removed mongodb require statements and mongodb routes add them to the mongodb file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-const db = require('../database/index.js');
 const cors = require('cors');
-const {getByID, addTechSpec, deleteTechSpec, updateTechSpec} = require('../database/dbhelpers')
-const {getByIdSQL} = require('../database/SQL/index')
+const {getByIdSQL, addTechSpecSQL, deleteTechSpecSQL} = require('../database/index')
 
 
 
@@ -20,19 +18,9 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 /* SERVER FUNCTIONS FOR HANDLING THE ROUTES */
 
-app.get('/api/product/:id', (req, res) => {
-  let { id } = req.params;
-  getByID({id})
-  .then((data) => {
-    res.status(200).send(data);
-  })
-  .catch((error) => {
-    res.status(404).send(error);
-  });
-})
 
-app.get('/api/productsql/:id', (req, res) => {
-  let { id } = req.params;
+app.get('/api/product/:id', (req, res) => {
+  let {id} = req.params;
   getByIdSQL(id)
   .then((data) => {
     res.status(200).send(data.rows);
@@ -43,28 +31,28 @@ app.get('/api/productsql/:id', (req, res) => {
 })
 
 
-
 app.post('/api/product', (req, res) => {
-  let { techSpec } = req.params;
-  addTechSpec({ techSpec })
+  console.log(req.body)
+  let {id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care} = req.body;
+  addTechSpecSQL(id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care)
   .then(() => {
-    res.status(200).send('data added!');
+    res.status(201).send('data added!');
   })
   .catch((error) => {
     res.status(404).send(error);
   })
-});
+})
 
 
-app.delete('/api/product/:id', (req, res) => {
-  let { id } = req.params;
-  deleteTechSpec(id)
-  .then(() => {
-    res.status(200).send('document deleted');
-  })
-  .catch((error) => {
-    res.status(404).send(error);
-  })
-});
+app.delete('/api/product/:id', (req,res) => {
+  let {id} = req.params;
+  deleteTechSpecSQL(id)
+    .then(() => {
+      res.status(200).send('document deleted')
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    })
+})
 
 app.listen(port, () => {console.log('server listening on port' , port)});
