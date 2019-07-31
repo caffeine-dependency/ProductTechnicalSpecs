21c280b99603af1c0a8c3cff157ee5cb0ac37f76
Sam Joseph <sjoseph92@gmail.com>
2019-07-31 16:32:08 -0700

    removed original schema file and replaced with pgschema file

---
+++ pgschema.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS pumpSquad2;
+CREATE DATABASE pumpSquad2;
+
+\connect pumpsquad2
+
+
+
+CREATE TABLE techspecs (
+  productid int PRIMARY KEY,
+  technicalFeatures text[] NOT NULL,
+  designAndFit text NOT NULL,
+  zippersAndFly text NOT NULL,
+  pocketConfig text NOT NULL,
+  construction text[] NOT NULL,
+  collarConfig text[] NOT NULL,
+  hemConfig text NOT NULL,
+  fabricTreatment text NOT NULL,
+  materials text[] NOT NULL,
+  care text[] NOT NULL
+);
+
+
+
+CREATE TABLE imgcarousel (
+  productid int REFERENCES techspecs(productid),
+  imageurl text NOT NULL,
+  username text NOT NULL,
+  imgcaption text NOT NULL
+); 
+
+CREATE TABLE question (
+  id int PRIMARY KEY,
+  productid int REFERENCES techspecs(productid),
+  questionHeader text NOT NULL,
+  questionContent text NOT NULL,
+  questionDate date NOT NULL,
+  thumbsUp smallint NOT NULL,
+  thumbsDown smallint NOT NULL,
+  username text NOT NULL,
+  userLocation text NOT NULL,
+  userAge smallint NOT NULL,
+  userActivity text NOT NULL
+);
+
+CREATE TABLE answers (
+  questionid int REFERENCES question(id),
+  answer text NOT NULL,
+  answerDate date NOT NULL,
+  thumbsUp smallint NOT NULL,
+  thumbsDown smallint NOT NULL,
+  username text NOT NULL,
+  userLocation text NOT NULL,
+  userAge smallint NOT NULL,
+  userActivity text NOT NULL
+);
+select techspecs.*, array_agg('[' || imgcarousel.imageurl || ',' || imgcarousel.username || ',' || imgcarousel.imgcaption || ']') as userimages  from imgcarousel inner join techspecs on techspecs.productid = imgcarousel.productid group by techspecs.productid;
+
+
+CREATE MATERIALIZED VIEW productpage as select techspecs.*, array_agg(string_to_array(imgcarousel.imageurl || ',' || imgcarousel.username || ',' || imgcarousel.imgcaption,',')) as userimages  from imgcarousel inner join techspecs on techspecs.productid = imgcarousel.productid group by techspecs.productid;
