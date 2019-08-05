53409b8889bdb8f863df39575478a2d9919c04af
Sam Joseph <sjoseph92@gmail.com>
2019-08-05 13:28:17 -0700

    seperating connecting to database and dbhelpers

--- database/index.js
+++ database/index.js
@@ -1,29 +1,8 @@
 var pg = require('pg');
-var conString = "postgres://samjoseph@localhost:5432/pumpsquad2";
+var conString = "postgres://samjoseph:***REMOVED***@localhost:5432/pumpsquad2";
 
 var client = new pg.Client(conString);
+
 client.connect()
 
-const getByIdSQL = (id) => {
-    return client.query(`SELECT * from productpage where productid=10000000`)
-}
-
-const addTechSpecSQL = (id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care) => {
-    return client.query(`INSERT INTO techspecs (id,technicalFeatures,designAndFit,zippersAndFly,pocketConfig,construction,collarConfig,hemConfig,fabricTreatment,materials,care) VALUES (${id},'{${technicalFeatures}}','${designAndFit}','${zippersAndFly}','${pocketConfig}','{${construction}}','{${collarConfig}}','${hemConfig}','${fabricTreatment}','{${materials}}','{${care}}')`)
-}
-
-
-
-const deleteTechSpecSQL = (id) => {
-    return client.query(`delete from techspecs where id=${id}`)
-}
-
-
-
-
-module.exports = {
-    getByIdSQL,
-    addTechSpecSQL,
-    deleteTechSpecSQL
-}
-
+module.exports = client
