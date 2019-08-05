952526a4bb0ee3dc5e9c08efaddc19618b0701a4
Sam Joseph <sjoseph92@gmail.com>
2019-08-05 16:28:51 -0700

    removed password etc, will add once cloned to instance

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 var pg = require('pg');
-var conString = "postgres://samjoseph:***REMOVED***@localhost:5432/pumpsquad2";
+var conString = "postgres://username:password@host/pumpsquad2";
 
 var client = new pg.Client(conString);
 
